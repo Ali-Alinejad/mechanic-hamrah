252d7f5993e45084ca24e9ade2b4925f254f0265
Ali-Alinejad <winford.op@gmail.com>
2024-10-08 07:36:48 +0330

    set navbar

---
+++ nextjs-test/app/blog/page.js
@@ -0,0 +1,9 @@
+function page() {
+    return (
+        <div>
+            blog
+        </div>
+    )
+}
+
+export default page
--- nextjs-test/app/components/Navigation.js
+++ nextjs-test/app/components/Navigation.js
@@ -5,39 +5,26 @@ import Link from "next/link";
 
 function Navigation() {
   return (
-    <nav className="bg-blue-600 fixed w-full top-0 left-0 shadow-lg z-50">
-      <div className="container mx-auto flex justify-between items-center p-4">
-         <Link href="/"
-             className="text-white hover:bg-blue-700 px-3 py-2 rounded transition duration-300">
-        <h1 className="text-white text-2xl font-bold">سوپراپلیکیشن</h1>
-          </Link>
-        <div className="flex space-x-4">
-         
-         
-            
-          <Link href="/car"
-             className="text-white hover:bg-blue-700 px-3 py-2 rounded transition duration-300">
-              درخواست خودرو
-          
-          </Link>
-          <Link href="/food"
-            className="text-white hover:bg-blue-700 px-3 py-2 rounded transition duration-300">
-              سفارش غذا
-      
-          </Link>
-          <Link href="/shopping"
-             className="text-white hover:bg-blue-700 px-3 py-2 rounded transition duration-300">
-              خدمات
-     
-          </Link>
-          <Link href="/account"
-             className="bg-yellow-500 text-white hover:bg-yellow-600 px-4 py-2 rounded transition duration-300">
-              ثبت‌نام
-           
-          </Link>
-        </div>
-      </div>
-    </nav>
+    <>
+      <li className="flex justify-around p-10 shadow-md">
+        <ul className="p-4">لوگو شرکتی</ul>
+
+        <Link href="/">
+          <ul className="p-4">خانه</ul>
+        </Link>
+        <Link href="/cabin">
+          <ul className="p-4">داشبورد</ul>
+        </Link>
+        <Link href="/blog">
+          <ul className="p-4">بلاگ</ul>
+        </Link>
+        <Link href="/account">
+          <ul className="border-2 rounded-md bg-cyan-700 text-white p-4 w-40 text-center hover:scale-105 transition duration-200  hover:ring-2">
+            ورود
+          </ul>
+        </Link>
+      </li>
+    </>
   );
 }
 
