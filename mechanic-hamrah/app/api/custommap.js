c2b018dc2088228325defff6fea8148512b11f56
Ali-Alinejad <winford.op@gmail.com>
2024-10-15 17:09:58 +0330

    add distance

--- mechanic-hamrah/app/api/custommap.js
+++ mechanic-hamrah/app/api/custommap.js
@@ -1,13 +1,7 @@
 "use client";
 import { supabase } from "../SupaBase/supabaseClient";
 import { useEffect, useState } from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  useMapEvents,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Button, Spinner } from "@nextui-org/react";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
@@ -34,7 +28,23 @@ const customIcon = new L.Icon({
   popupAnchor: [0, -35],
 });
 
+// تابع محاسبه فاصله
+const getDistance = (lat1, lon1, lat2, lon2) => {
+  const R = 6371; // شعاع زمین به کیلومتر
+  const dLat = (lat2 - lat1) * (Math.PI / 180);
+  const dLon = (lon2 - lon1) * (Math.PI / 180);
 
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1 * (Math.PI / 180)) *
+      Math.cos(lat2 * (Math.PI / 180)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c; // فاصله به کیلومتر
+};
 
 function MapIrMap({ onClick }) {
   const [location, setLocation] = useState(null);
@@ -63,7 +73,7 @@ function MapIrMap({ onClick }) {
   };
 
   const fetchLocations = async () => {
-   let { data, error } = await supabase.from("Type").select("*");
+    let { data, error } = await supabase.from("Type").select("*");
     if (error) {
       console.error("Error fetching locations:", error);
     } else {
@@ -102,56 +112,77 @@ function MapIrMap({ onClick }) {
             </Marker>
           )}
 
-          {locations.map((loc) => (
-            <Marker
-              key={loc.id}
-              position={[loc.lat, loc.lon]}
-              icon={customIcon}
-            >
-              <Popup>
-                <div className=" w-fit">
-                  <div className="shadow-lg rounded-lg bg-white">
-                    <div className="bg-blue-600 text-white rounded-t-lg p-4">
-                      <h3 className="text-lg font-semibold">{loc.name}</h3>
-                    </div>
-                    <div className="p-4">
-                      <p className="text-gray-700 mb-2">آدرس: {loc.address}</p>
-                      <div className="flex items-center justify-between mt-2 mb-2">
-                        <span className="text-gray-600 font-medium">تلفن:</span>
-                        <input
-                          className="w-full border border-gray-300 rounded-md p-2"
-                          value={loc.status ? loc.phone : "کاربر غیر فعال است"}
-                          readOnly
-                        />
+          {locations.map((loc) => {
+            // محاسبه فاصله
+            const distance = getDistance(
+              location.latitude,
+              location.longitude,
+              loc.lat,
+              loc.lon
+            ).toFixed(2);
+
+            return (
+              <Marker
+                key={loc.id}
+                position={[loc.lat, loc.lon]}
+                icon={customIcon}
+              >
+                <Popup>
+                  <div className="w-fit">
+                    <div className="shadow-lg rounded-lg bg-white">
+                      <div className="bg-blue-600 text-white rounded-t-lg p-4">
+                        <h3 className="text-lg font-semibold">{loc.name}</h3>
                       </div>
-                      <div className="flex items-center justify-between bg-blue-50 p-2 rounded-md shadow-sm mb-2">
-                        <span className="text-blue-600 font-bold">نوع:</span>
-                        <span className="bg-blue-200 text-blue-800 px-2 py-1 rounded-full">
-                          {loc.type}
-                        </span>
-                      </div>
-                      <div
-                        className={`mt-2 p-2 rounded-md shadow-sm ${
-                          loc.status ? "bg-green-50" : "bg-red-50"
-                        }`}
-                      >
-                        <div className="flex justify-around w-full">
-                          <span>⭐{loc.score}</span>
-                          <span
-                            className={`font-bold ${
-                              loc.status ? "text-green-600" : "text-red-600"
-                            }`}
-                          >
-                            {loc.status ? "فعال" : "غیر فعال"}
+                      <div className="p-4">
+                        <p className="text-gray-700 mb-2">
+                          آدرس: {loc.address}
+                        </p>
+
+                        <div className="flex items-center justify-between mt-2 mb-2">
+                          <span className="text-gray-600 font-medium">
+                            تلفن:
+                          </span>
+                          <input
+                            className="w-full border border-gray-300 rounded-md p-2"
+                            value={
+                              loc.status ? loc.phone : "کاربر غیر فعال است"
+                            }
+                            readOnly
+                          />
+                        </div>
+                        <div className="flex items-center justify-between bg-blue-50 p-2 rounded-md shadow-sm mb-2">
+                          <span className="text-blue-600 font-bold">نوع:</span>
+                          <span className="bg-blue-200 text-blue-800 px-2 py-1 rounded-full">
+                            {loc.type}
                           </span>
                         </div>
+                        <div
+                          className={`mt-2 p-2 rounded-md shadow-sm ${
+                            loc.status ? "bg-green-50" : "bg-red-50"
+                          }`}
+                        >
+                          <div className="flex justify-around w-full">
+                            <span> ⭐{loc.score}</span>
+                            <span
+                              className={`font-bold ${
+                                loc.status ? "text-green-600" : "text-red-600"
+                              }`}
+                            >
+                              {loc.status ? "فعال" : "غیر فعال"}
+                            </span>
+                          </div>
+                        </div>
+                        <p className="text-gray-700 p-1">
+                          فاصله تا شما: {distance < 1 ? distance * 1000 : Math.round(distance)} {" "}
+                          {distance < 1 ? "متر" : "کیلومتر"}
+                        </p>
                       </div>
                     </div>
                   </div>
-                </div>
-              </Popup>
-            </Marker>
-          ))}
+                </Popup>
+              </Marker>
+            );
+          })}
         </MapContainer>
       )}
       <div className="flex-row-reverse flex">
