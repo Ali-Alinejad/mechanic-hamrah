874d2e4ea2657eee33b3d7ea74483c411484d7c7
Ali-Alinejad <winford.op@gmail.com>
2024-10-22 14:40:06 +0330

    add market with loc in add page

--- mechanic-hamrah/app/add/page.js
+++ mechanic-hamrah/app/add/page.js
@@ -1,17 +1,37 @@
 "use client";
 
-import MapIrMap from "../api/custommap";
-import Navigation from "../components/Navigation";
-import { Button, Input, Textarea } from "@nextui-org/react";
 import { useState } from "react";
+import { Button, Input, Textarea } from "@nextui-org/react";
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+import Navigation from "../components/Navigation";
 
+function LocationMarker({ setLatlng, setSelectedLocation }) {
+  useMapEvents({
+    click(e) {
+      const { lat, lng } = e.latlng;
+      setLatlng({ lat, lng });
+      setSelectedLocation({ lat, lng });
+    },
+  });
+
+  return null;
+}
+const customIcon = new L.Icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+  iconSize: [35, 35],
+  iconAnchor: [17, 35],
+  popupAnchor: [0, -35],
+});
 function Page() {
   const [selectedLocation, setSelectedLocation] = useState(null);
+  const [shopName, setShopName] = useState("");
+  const [shopNumber, setShopNumber] = useState("");
+  const [workingHours, setWorkingHours] = useState("");
+  const [rating, setRating] = useState("");
+  const [latlng, setLatlng] = useState({ lat: "", lng: "" });
 
-  const handleMapClick = (lat, lng) => {
-    setSelectedLocation({ lat, lng });
-    console.log(selectedLocation);
-
+  const isFormValid = () => {
+    return shopName && shopNumber && workingHours && rating && latlng.lat && latlng.lng;
   };
 
   return (
@@ -20,68 +40,98 @@ function Page() {
       <div className="flex justify-center items-start bg-gradient-to-tr from-blue-600 to-violet-600 min-h-screen w-full">
         <div className="flex flex-row-reverse w-full max-w-6xl mx-auto space-x-4">
           <div className="w-[40%] m-4 p-6 bg-white rounded-lg shadow-xl rtl">
-            <h1 className="text-3xl font-bold mb-6 text-gray-800">
-              اطلاعات مغازه
-            </h1>
+            <h1 className="text-3xl font-bold mb-6 text-gray-800">اطلاعات مغازه</h1>
             <form className="space-y-6">
-              <div>
-                <Input
-                  clearable
-                  underlined
-                  label="نام مغازه"
-                  placeholder="نام مغازه را وارد کنید"
-                  required
-                />
-              </div>
-              <div>
-                <Input
-                  clearable
-                  underlined
-                  label="شماره مغازه"
-                  placeholder="شماره مغازه را وارد کنید"
-                  required
-                />
-              </div>
-              <div>
-                <Textarea
-                  label="ساعت فعالیت"
-                  placeholder="ساعات فعالیت را وارد کنید"
-                  required
-                />
-              </div>
-              <div>
+              <Input
+                clearable
+                underlined
+                label="نام مغازه"
+                placeholder="نام مغازه را وارد کنید"
+                required
+                value={shopName}
+                onChange={(e) => setShopName(e.target.value)}
+              />
+              <Input
+                clearable
+                underlined
+                label="شماره مغازه"
+                placeholder="شماره مغازه را وارد کنید"
+                required
+                value={shopNumber}
+                onChange={(e) => setShopNumber(e.target.value)}
+              />
+              <Textarea
+                label="ساعت فعالیت"
+                placeholder="ساعات فعالیت را وارد کنید"
+                required
+                value={workingHours}
+                onChange={(e) => setWorkingHours(e.target.value)}
+              />
+              <Input
+                type="number"
+                clearable
+                underlined
+                label="امتیاز دهی"
+                placeholder="امتیاز خود را وارد کنید (0 تا 5)"
+                min={0}
+                max={5}
+                required
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+              />
+              <div className="flex flex-row gap-1">
                 <Input
                   type="number"
                   clearable
                   underlined
-                  label="امتیاز دهی"
-                  placeholder="امتیاز خود را وارد کنید (0 تا 5)"
-                  min={0}
-                  max={5}
-                  required
+                  label="عرض جغرافیایی"
+                  placeholder="35.1024"
+                  readOnly
+                  value={latlng.lat}
+                />
+                <Input
+                  type="number"
+                  clearable
+                  underlined
+                  label="طول جغرافیایی"
+                  placeholder="51.1024"
+                  readOnly
+                  value={latlng.lng}
                 />
               </div>
-              <Button type="submit" className="w-full mt-6" color="primary">
+              <Button 
+                type="submit" 
+                className="w-full mt-6" 
+                color="primary"
+                isDisabled={!isFormValid()}
+              >
                 ذخیره اطلاعات
               </Button>
             </form>
-            {selectedLocation && (
-              <div className="mt-6 p-4 bg-blue-100 rounded-lg shadow">
-                <p>
-                  <strong>عرض جغرافیایی:</strong> {selectedLocation.lat}
-                </p>
-                <p>
-                  <strong>طول جغرافیایی:</strong> {selectedLocation.lng}
-                </p>
-              </div>
-            )}
           </div>
           <div className="w-[60%] ml-2">
             <div className="h-fit bg-gray-300 rounded-lg mt-4">
-              <MapIrMap
-                onClick={handleMapClick}
-                selectedLocation={selectedLocation}
-              />
+              <MapContainer
+                center={[35.661521, 51.395466]}
+                zoom={15}
+                className="h-[70vh] w-[100%] overflow-hidden"
+              >
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                
+                {selectedLocation && (
+                  <Marker position={selectedLocation} icon={customIcon}>
+                    <Popup>
+                      مغازه در اینجا قرار دارد.
+                    </Popup>
+                  </Marker>
+        
+
+                )}
+                
+                {/* افزودن کامپوننت LocationMarker */}
+                <LocationMarker setLatlng={setLatlng} setSelectedLocation={setSelectedLocation} />
+                
+              </MapContainer>
             </div>
           </div>
         </div>
@@ -89,4 +139,5 @@ function Page() {
     </>
   );
 }
+
 export default Page;
--- mechanic-hamrah/app/api/custommap.js
+++ mechanic-hamrah/app/api/custommap.js
@@ -1,12 +1,11 @@
 "use client";
 import { supabase } from "../SupaBase/supabaseClient";
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Button, Spinner } from "@nextui-org/react";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-// Default marker icon settings
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
@@ -14,15 +13,14 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
-// User location icon
 const userLocationIcon = new L.Icon({
-  iconUrl: "https://www.iconpacks.net/icons/2/free-location-icon-2955-thumb.png",
+  iconUrl:
+    "https://www.iconpacks.net/icons/2/free-location-icon-2955-thumb.png",
   iconSize: [35, 35],
   iconAnchor: [17, 35],
   popupAnchor: [0, -35],
 });
 
-// Custom marker icon
 const customIcon = new L.Icon({
   iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
   iconSize: [35, 35],
@@ -30,174 +28,175 @@ const customIcon = new L.Icon({
   popupAnchor: [0, -35],
 });
 
-// Function to calculate distance between two geographical points
+
 const getDistance = (lat1, lon1, lat2, lon2) => {
-  const R = 6371; // Radius of the Earth in km
+  const R = 6371; 
   const dLat = (lat2 - lat1) * (Math.PI / 180);
   const dLon = (lon2 - lon1) * (Math.PI / 180);
+
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(lat1 * (Math.PI / 180)) *
       Math.cos(lat2 * (Math.PI / 180)) *
       Math.sin(dLon / 2) *
       Math.sin(dLon / 2);
+
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return R * c; // Distance in kilometers
+
+  return R * c; 
 };
 
-// Map event handler component
-function MapEventHandler({ onClick }) {
-    useMapEvents({
-        click: (event) => {
-            onClick(event);
+function MapIrMap({ onClick }) {
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [locations, setLocations] = useState([]);
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setLocation({ latitude, longitude });
+          setLoading(false);
         },
-    });
-    return null;
-}
-
-// Main map component
-function MapIrMap() {
-    const [location, setLocation] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
-    const [locations, setLocations] = useState([]);
-    const [clickedPosition, setClickedPosition] = useState(null);
-    const [distanceToMarker, setDistanceToMarker] = useState(null); // State to hold distance to clicked marker
-
-    // Get user's current location
-    const handleLocationClick = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const { latitude, longitude } = position.coords;
-                    setLocation({ latitude, longitude });
-                    setLoading(false);
-                },
-                (error) => {
-                    console.error("خطا در دریافت موقعیت جغرافیایی:", error);
-                    setError("خطا در دریافت موقعیت جغرافیایی.");
-                    setLoading(false);
-                }
-            );
-        } else {
-            setError("Geolocation is not supported by this browser.");
-            setLoading(false);
+        (error) => {
+          console.error("خطا در دریافت موقعیت جغرافیایی:", error);
+          setError("خطا در دریافت موقعیت جغرافیایی.");
+          setLoading(false);
         }
-    };
+      );
+    } else {
+      setError("Geolocation is not supported by this browser.");
+      setLoading(false);
+    }
+  };
 
-    // Fetch locations from Supabase
-    const fetchLocations = async () => {
-        let { data, error } = await supabase.from("Type").select("*");
-        if (error) {
-            console.error("Error fetching locations:", error);
-        } else {
-            setLocations(data);
-        }
-    };
+  const fetchLocations = async () => {
+    let { data, error } = await supabase.from("Type").select("*");
+    if (error) {
+      console.error("Error fetching locations:", error);
+    } else {
+      console.log("Fetched locations:", data);
+      setLocations(data);
+    }
+  };
 
-    // Handle map click event
-    const handleMapClick = (event) => {
-        const { latlng } = event; // Get latlng directly from event
-        console.log(`Clicked at: ${latlng.lat}, ${latlng.lng}`); // Log coordinates
-        setClickedPosition(latlng); // Set clicked position
-        setDistanceToMarker(null);
-    };
+  useEffect(() => {
+    handleLocationClick();
+    fetchLocations();
+  }, []);
 
-    // Handle marker click event
-    const handleMarkerClick = (loc) => {
-        if (location) {
-            const distance = getDistance(location.latitude, location.longitude, loc.lat, loc.lon);
-            setDistanceToMarker(distance.toFixed(2)); // Set distance to clicked marker
-            console.log(`Distance to ${loc.name}: ${distance.toFixed(2)} km`);
-        }
-    };
-
-    useEffect(() => {
-        handleLocationClick();
-        fetchLocations();
-    }, []);
-
-    return (
-        <div className="relative p-4 text-center justify-center">
-            {loading ? (
-                <div className="flex justify-center items-center" style={{ height: "70vh" }}>
-                    <Spinner size="lg" />
-                </div>
-            ) : (
-                <MapContainer
-                    center={[location.latitude, location.longitude]}
-                    zoom={15}
-                    style={{ height: "70vh", width: "100%" }}
-                >
-                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                    
-                    <MapEventHandler onClick={handleMapClick} /> {/* Add event handler */}
-
-                    {location && (
-                        <Marker position={[location.latitude, location.longitude]} icon={userLocationIcon}>
-                            <Popup>موقعیت شما</Popup>
-                        </Marker>
-                    )}
-
-                    {locations.map((loc) => (
-                        <Marker key={loc.id} position={[loc.lat, loc.lon]} icon={customIcon} onClick={() => handleMarkerClick(loc)}>
-                            <Popup>
-                                <div className="w-fit">
-                                    <div className="shadow-lg rounded-lg bg-white">
-                                        <div className="bg-blue-600 text-white rounded-t-lg p-4">
-                                            <h3 className="text-lg font-semibold">{loc.name}</h3>
-                                        </div>
-                                        <div className="p-4">
-                                            <p className="text-gray-700 mb-2">آدرس: {loc.address}</p>
-                                            <div className="flex items-center justify-between mt-2 mb-2">
-                                                <span className="text-gray-600 font-medium">تلفن:</span>
-                                                <input
-                                                    className="w-full border border-gray-300 rounded-md p-2"
-                                                    value={loc.status ? loc.phone : "کاربر غیر فعال است"}
-                                                    readOnly
-                                                />
-                                            </div>
-                                            <div className="flex items-center justify-between bg-blue-50 p-2 rounded-md shadow-sm mb-2">
-                                                <span className="text-blue-600 font-bold">نوع:</span>
-                                                <span className="bg-blue-200 text-blue-800 px-2 py-1 rounded-full">
-                                                    {loc.type}
-                                                </span>
-                                            </div>
-                                            {/* Star Rating */}
-                                            <div className="flex justify-around w-full">
-                                                <span> ⭐{loc.score}</span>
-                                                <span className={`font-bold ${loc.status ? "text-green-600" : "text-red-600"}`}>
-                                                    {loc.status ? "فعال" : " غیر فعال"}
-                                                </span>
-                                            </div>
-
-                                            {/* Distance Display */}
-                                            <p className="text-gray-700 p-1">
-                                                فاصله تا شما: {distanceToMarker && distanceToMarker < 1 ? (distanceToMarker * 1000).toFixed(0) : Math.round(distanceToMarker)}{" "}
-                                                {distanceToMarker && distanceToMarker < 1 ? "متر" : "کیلومتر"}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Popup>
-                        </Marker>
-                    ))}
-                    
-                    {clickedPosition && (
-                        <Marker position={[clickedPosition.lat, clickedPosition.lng]} icon={customIcon}>
-                            <Popup>نقطه انتخاب شده<br />{`Lat: ${clickedPosition.lat}, Lng: ${clickedPosition.lng}`}</Popup>
-                        </Marker>
-                    )}
-                </MapContainer>
-            )}
-            <div className="flex-row-reverse flex">
-                <Button onClick={handleLocationClick} className="w-12 h-12 shadow-2xl p-2 overflow-hidden rounded-full" color="primary">
-                    موقعیت شما
-                </Button>
-            </div>
-            {error && <div className="text-red-500 mt-4">{error}</div>}
+  return (
+    <div className="relative p-4 text-center justify-center">
+      {loading ? (
+        <div
+          className="flex justify-center items-center"
+          style={{ height: "70vh" }}
+        >
+          <Spinner size="lg" />
         </div>
-    );
+      ) : (
+        <MapContainer
+          center={[location.latitude, location.longitude]}
+          zoom={15}
+          style={{ height: "70vh", width: "100%" }}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          {location && (
+            <Marker
+              position={[location.latitude, location.longitude]}
+              icon={userLocationIcon}
+            >
+              <Popup>موقعیت شما</Popup>
+            </Marker>
+          )}
+
+          {locations.map((loc) => {
+            // محاسبه فاصله
+            const distance = getDistance(
+              location.latitude,
+              location.longitude,
+              loc.lat,
+              loc.lon
+            ).toFixed(2);
+
+            return (
+              <Marker
+                key={loc.id}
+                position={[loc.lat, loc.lon]}
+                icon={customIcon}
+              >
+                <Popup>
+                  <div className="w-fit">
+                    <div className="shadow-lg rounded-lg bg-white">
+                      <div className="bg-blue-600 text-white rounded-t-lg p-4">
+                        <h3 className="text-lg font-semibold">{loc.name}</h3>
+                      </div>
+                      <div className="p-4">
+                        <p className="text-gray-700 mb-2">
+                          آدرس: {loc.address}
+                        </p>
+
+                        <div className="flex items-center justify-between mt-2 mb-2">
+                          <span className="text-gray-600 font-medium">
+                            تلفن:
+                          </span>
+                          <input
+                            className="w-full border border-gray-300 rounded-md p-2"
+                            value={
+                              loc.status ? loc.phone : "کاربر غیر فعال است"
+                            }
+                            readOnly
+                          />
+                        </div>
+                        <div className="flex items-center justify-between bg-blue-50 p-2 rounded-md shadow-sm mb-2">
+                          <span className="text-blue-600 font-bold">نوع:</span>
+                          <span className="bg-blue-200 text-blue-800 px-2 py-1 rounded-full">
+                            {loc.type}
+                          </span>
+                        </div>
+                        <div
+                          className={`mt-2 p-2 rounded-md shadow-sm ${
+                            loc.status ? "bg-green-50" : "bg-red-50"
+                          }`}
+                        >
+                          <div className="flex justify-around w-full">
+                            <span> ⭐{loc.score}</span>
+                            <span
+                              className={`font-bold ${
+                                loc.status ? "text-green-600" : "text-red-600"
+                              }`}
+                            >
+                              {loc.status ? "فعال" : " غیر فعال"}
+                            </span>
+                          </div>
+                        </div>
+                        <p className="text-gray-700 p-1">
+                          فاصله تا شما: {distance < 1 ? distance * 1000 : Math.round(distance)} {" "}
+                          {distance < 1 ? "متر" : "کیلومتر"}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </Popup>
+              </Marker>
+            );
+          })}
+        </MapContainer>
+      )}
+      <div className="flex-row-reverse flex">
+        <Button
+          onClick={handleLocationClick}
+          className="w-12 h-12 shadow-2xl p-2 overflow-hidden rounded-full"
+          color="primary"
+        >
+          موقعیت شما
+        </Button>
+      </div>
+      {error && <div className="text-red-500 mt-4">{error}</div>}
+    </div>
+  );
 }
 
 export default MapIrMap;
